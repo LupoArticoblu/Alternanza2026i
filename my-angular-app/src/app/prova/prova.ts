6c8bdffce1420c6fded37ae3a39340c4a03b2832
LupoArticoblu <wolf94-@libero.it>
2026-02-13 14:39:24 +0100

    installazione angular, primo components e installazione angular materials

---
+++ my-angular-app/src/app/prova/prova.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MatSliderModule } from '@angular/material/slider';
+
+@Component({
+  selector: 'app-prova',
+  imports: [MatSliderModule],
+  templateUrl: './prova.html',
+  styleUrl: './prova.scss',
+})
+export class Prova {
+
+}
+
+
--- my-angular-app/src/main.ts
+++ my-angular-app/src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { Component } from '@angular/core'
 import { appConfig } from './app/app.config';
-import { App } from './app/app';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  template: '<h2>Welcome in Angular! {{name}}</h2>',
+})
+class App{
+  name = 'Carmelo';
+}
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
