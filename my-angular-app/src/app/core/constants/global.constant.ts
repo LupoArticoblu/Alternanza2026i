e1038a816f87b951a76790e76ecfdb8acfb7074e
LupoArticoblu <wolf94-@libero.it>
2026-02-16 15:27:32 +0100

    CONSTANT: global e validation

--- my-angular-app/src/app/core/constants/global.constant.ts
+++ my-angular-app/src/app/core/constants/global.constant.ts
@@ -1,6 +1,13 @@
+import e from "express";
+
 export const GlobalConstant ={
     API_END_POINT: {
         HOTEL: '/hotels',
         PARENT: '/STRUCTURE'
+    },
+    REGULAR_EXPRESSION: {
+        AADHAR_CARD: '',
+        PAN_CARD: '',
+        eMAIL: '',
     }
 }
---
+++ my-angular-app/src/app/core/constants/validation.constant.ts
@@ -0,0 +1,5 @@
+export const validationConstant ={
+    REQUIRED: 'This field is required.',
+    INVALID_EMAIL: 'Please enter a valid email address.',
+
+}
