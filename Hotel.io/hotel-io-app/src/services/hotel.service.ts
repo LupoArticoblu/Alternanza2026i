c1c8d29006f92c909b19dd801bd2a009580a92a1
LupoArticoblu <wolf94-@libero.it>
2026-03-04 16:03:36 +0100

    implementare ollama in hotel.service

--- Hotel.io/hotel-io-app/src/services/hotel.service.ts
+++ Hotel.io/hotel-io-app/src/services/hotel.service.ts
@@ -1,6 +1,6 @@
-import { Injectable, signal, computed, inject } from '@angular/core';
-import { GoogleGenAI, Type } from '@google/genai';
+import { Injectable, signal, computed, inject, model } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { hasSubscribers } from 'diagnostics_channel';
 
 export interface Review {
   id: string;
@@ -137,53 +137,94 @@ export class HotelService {
       hotels.map(h => h.id === hotelId ? { ...h, isAnalyzing: true } : h)
     );
 
-    try {
-      const ai = new GoogleGenAI({ apiKey: process.env.API_KEY });
-      const reviewsText = hotel.reviews.map(r => `"${r.comment}" (Rating: ${r.rating}/5)`).join('\n');
+    // try {
+    //   const ai = new GoogleGenAI({ apiKey: process.env.API_KEY });
+    //   const reviewsText = hotel.reviews.map(r => `"${r.comment}" (Rating: ${r.rating}/5)`).join('\n');
       
+    //   const prompt = `
+    //     You are an expert travel critic. Analyze the following reviews for the hotel "${hotel.name}".
+    //     Provide a concise summary, a calculated overall score out of 10 based on sentiment, 
+    //     a list of key strengths, and a list of key weaknesses.
+        
+    //     Reviews:
+    //     ${reviewsText}
+    //   `;
+
+    //   const response = await ai.models.generateContent({
+    //     model: 'gemini-2.5-flash',
+    //     contents: prompt,
+    //     config: {
+    //       responseMimeType: 'application/json',
+    //       responseSchema: {
+    //         type: Type.OBJECT,
+    //         properties: {
+    //           summary: { type: Type.STRING },
+    //           score: { type: Type.NUMBER },
+    //           strengths: { type: Type.ARRAY, items: { type: Type.STRING } },
+    //           weaknesses: { type: Type.ARRAY, items: { type: Type.STRING } },
+    //         },
+    //         required: ["summary", "score", "strengths", "weaknesses"]
+    //       }
+    //     }
+    //   });
+
+    //   const jsonText = response.text;
+    //   if (jsonText) {
+    //     const analysis: AIAnalysis = JSON.parse(jsonText);
+        
+    //     this.hotelsSignal.update(hotels =>
+    //       hotels.map(h => h.id === hotelId ? { ...h, aiAnalysis: analysis, isAnalyzing: false } : h)
+    //     );
+    //   } else {
+    //     throw new Error('No response from AI');
+    //   }
+
+    // } catch (error) {
+    //   console.error('AI Analysis failed', error);
+    //   this.hotelsSignal.update(hotels =>
+    //     hotels.map(h => h.id === hotelId ? { ...h, isAnalyzing: false } : h)
+    //   );
+    // }
+
+    /*usiamo ollama per la logica del tasto summary*/
+    try{
+      const reviewsText = hotel.reviews.map(r => `${r.user}: ${r.comment}: ${r.rating}`).join('\n');
       const prompt = `
-        You are an expert travel critic. Analyze the following reviews for the hotel "${hotel.name}".
+        you are an expert travel critic. Analyze the following reviews for the hotel "${hotel.name}".
         Provide a concise summary, a calculated overall score out of 10 based on sentiment, 
         a list of key strengths, and a list of key weaknesses.
-        
+        WARNING! answer only in json format:
+        {
+          "summary": "concise summary",
+          "score": 10,
+          "strengths": ["strength1", "strength2"],
+          "weaknesses": ["weakness1", "weakness2"]
+        }
         Reviews:
         ${reviewsText}
-      `;
-
-      const response = await ai.models.generateContent({
-        model: 'gemini-2.5-flash',
-        contents: prompt,
-        config: {
-          responseMimeType: 'application/json',
-          responseSchema: {
-            type: Type.OBJECT,
-            properties: {
-              summary: { type: Type.STRING },
-              score: { type: Type.NUMBER },
-              strengths: { type: Type.ARRAY, items: { type: Type.STRING } },
-              weaknesses: { type: Type.ARRAY, items: { type: Type.STRING } },
-            },
-            required: ["summary", "score", "strengths", "weaknesses"]
-          }
+      `; 
+      const body = {
+        model: "mistral",
+        prompt: prompt,
+        stream: false,
+        format: "json"
+       };
+       this.http.post("http://localhost:11434/api/generate", body).subscribe({
+        next: (res: any) => {
+          //trasforma la stringa di ollama in oggetto
+          const analysis: AIAnalysis = JSON.parse(res.response);
+          //trova l'hotel e aggiorna analisi
+          this.hotelsSignal.update(hotels => hotels.map(h => h.id === hotelId ? { ...h, aiAnalysis: analysis, isAnalyzing: false}: h));
+        },
+        error: (err) =>{
+          console.error("Error analyzing reviews:", err);
+          //in caso di errore, imposta isAnalyzing a false
+          this.hotelsSignal.update(hotels => hotels.map(h => h.id === hotelId ? { ...h, isAnalyzing: false}: h));
         }
-      });
+       })
+    }
+    catch(error){
 
-      const jsonText = response.text;
-      if (jsonText) {
-        const analysis: AIAnalysis = JSON.parse(jsonText);
-        
-        this.hotelsSignal.update(hotels =>
-          hotels.map(h => h.id === hotelId ? { ...h, aiAnalysis: analysis, isAnalyzing: false } : h)
-        );
-      } else {
-        throw new Error('No response from AI');
-      }
-
-    } catch (error) {
-      console.error('AI Analysis failed', error);
-      this.hotelsSignal.update(hotels =>
-        hotels.map(h => h.id === hotelId ? { ...h, isAnalyzing: false } : h)
-      );
     }
   }
-}
+} 
